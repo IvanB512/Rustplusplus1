18108301a7bfcd4dce178fc62b86ff49867f095e
IvanB512 <ivan.belyaev.2003@bk.ru>
2026-03-21 09:56:00 +0300

    Add files via upload

--- config/index.js
+++ config/index.js
@@ -24,6 +24,7 @@ module.exports = {
         pollingIntervalMs: process.env.RPP_POLLING_INTERVAL || 10000,
         showCallStackError: process.env.RPP_LOG_CALL_STACK || false,
         reconnectIntervalMs: process.env.RPP_RECONNECT_INTERVAL || 15000,
+        openRouterApiKey: process.env.RPP_OPENROUTER_API_KEY || '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'
     },
     discord: {
         username: process.env.RPP_DISCORD_USERNAME || 'rustplusplus',
