8aede08897250a21db787f66fc16ee4db3794b08
IvanB512 <ivan.belyaev.2003@bk.ru>
2026-03-21 09:43:57 +0300

    Add files via upload

--- src/handlers/inGameCommandHandler.js
+++ src/handlers/inGameCommandHandler.js
@@ -21,6 +21,7 @@
 const SmartAlarmHandler = require('./smartAlarmHandler.js');
 const SmartSwitchGroupHandler = require('./smartSwitchGroupHandler.js');
 const SmartSwitchHandler = require('./smartSwitchHandler.js');
+const AiCommandHandler = require('./aiCommandHandler.js');
 
 module.exports = {
     inGameCommandHandler: async function (rustplus, client, message) {
@@ -42,6 +43,12 @@ module.exports = {
         }
         else if (!rustplus.generalSettings.inGameCommandsEnabled) {
             return false;
+        }
+        else if (commandLowerCase.startsWith(`${prefix}ai`)) {
+            if (await AiCommandHandler.aiCommandHandler(rustplus, client, command)) {
+                rustplus.logInGameCommand('AI', message);
+                return true;
+            }
         }
         else if (commandLowerCase === `${prefix}${client.intlGet('en', 'commandSyntaxAfk')}` ||
             commandLowerCase === `${prefix}${client.intlGet(guildId, 'commandSyntaxAfk')}`) {
@@ -206,6 +213,10 @@ module.exports = {
         else if (commandLowerCase === `${prefix}${client.intlGet('en', 'commandSyntaxWipe')}` ||
             commandLowerCase === `${prefix}${client.intlGet(guildId, 'commandSyntaxWipe')}`) {
             rustplus.sendInGameMessage(rustplus.getCommandWipe());
+        }
+        else if (commandLowerCase === `${prefix}${client.intlGet('en', 'commandSyntaxTravelingVendor')}` ||
+            commandLowerCase === `${prefix}${client.intlGet(guildId, 'commandSyntaxTravelingVendor')}`) {
+            rustplus.sendInGameMessage(rustplus.getCommandTravelingVendor());
         }
         else {
             /* Maybe a custom command? */
